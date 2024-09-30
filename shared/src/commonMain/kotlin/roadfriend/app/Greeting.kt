75f4f06527b8cc2b2921dce0a6786443cfb6ecf7
cuma.soyak <cuma.soyak@tomdigital.com.tr>
2024-09-30 18:33:38 +0300

    Revert "Initial commit"
    This reverts commit 8c4e0733860fceb07cc60f76e1843ba86ea8131e.

--- shared/src/commonMain/kotlin/roadfriend/app/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package roadfriend.app
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello cuma soyak, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/roadfriend/app/Platform.kt
+++
@@ -1,7 +0,0 @@
-package roadfriend.app
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
